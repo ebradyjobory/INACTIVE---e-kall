ff3b2f4fab4d3a8d5d358a833bfc850528f58e73
Essam Al Joubori <essam.aljoubori@gmail.com>
2013-12-02 12:09:04 -0600

    Added an about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ root 'pages#home'
   match '/service', to: 'pages#service', via: 'get'
   match '/about', to: 'pages#about', via: 'get'
   match '/contact', to: 'pages#contact', via: 'get'
+  match '/structural', to: 'pages#structure', via: 'get'
+  match '/michanical', to: 'pages#michanic', via: 'get'
+  match '/electrical', to: 'pages#electric', via: 'get'
+  match '/combat', to: 'pages#combat', via: 'get'
  
   
   # The priority is based upon order of creation: first created -> highest priority.
